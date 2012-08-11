2202e638bfaabfc986e7e488a8cd5ac0ba87dfbf
Vinicius Ferriani <viniciusnz@gmail.com>
2012-08-11 04:10:08 -0300

    Update lib/piet/carrierwave_extension.rb
    Don't know if MiniMagick part works, but the RMagick part now should... It was not processing the image in my previous attempt.

--- lib/piet/carrierwave_extension.rb
+++ lib/piet/carrierwave_extension.rb
@@ -2,14 +2,34 @@ module Piet
   module CarrierWaveExtension
     def optimize
       manipulate! do |img|
-        Piet.optimize(img.class.to_s == "Magick::Image" ? img.base_filename : img.path)
+        if img.class.to_s == "Magick::Image"
+          img.write(current_path)
+          Piet.optimize(current_path)
+          img = RMagick::Image.read(current_path) # reopen the img and return
+        elsif img.class.to_s == "MiniMagick::Image"
+          img.write(img.path)
+          Piet.optimize(img.path)
+          ::MiniMagick::Image.open(img.path) # reopen the img and return
+        else
+          Piet.optimize(img.path)        
+        end
         img
       end
     end
 
     def pngquant
       manipulate! do |img|
-        Piet.pngquant(img.class.to_s == "Magick::Image" ? img.base_filename : img.path)
+        if img.class.to_s == "Magick::Image"
+          img.write(current_path)
+          Piet.pngquant(current_path)
+          img = RMagick::Image.read(current_path) # reopen the img and return
+        elsif img.class.to_s == "MiniMagick::Image"
+          img.write(img.path)
+          Piet.pngquant(img.path)
+          ::MiniMagick::Image.open(img.path) # reopen the img and return
+        else
+          Piet.pngquant(img.path)
+        end
         img
       end
     end
