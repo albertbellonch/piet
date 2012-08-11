5adf2ce775e6ef782d0f517230e52fb2ab1dcae2
Vinicius Ferriani <viniciusnz@gmail.com>
2012-08-11 04:15:08 -0300

    Update lib/piet/carrierwave_extension.rb

--- lib/piet/carrierwave_extension.rb
+++ lib/piet/carrierwave_extension.rb
@@ -5,11 +5,11 @@ module Piet
         if img.class.to_s == "Magick::Image"
           img.write(current_path)
           Piet.optimize(current_path)
-          img = RMagick::Image.read(current_path) # reopen the img and return
+          img = ::RMagick::Image.read(current_path) # reopen the img and return
         elsif img.class.to_s == "MiniMagick::Image"
           img.write(img.path)
           Piet.optimize(img.path)
-          ::MiniMagick::Image.open(img.path) # reopen the img and return
+          img = ::MiniMagick::Image.open(img.path) # reopen the img and return
         else
           Piet.optimize(img.path)        
         end
@@ -22,11 +22,11 @@ module Piet
         if img.class.to_s == "Magick::Image"
           img.write(current_path)
           Piet.pngquant(current_path)
-          img = RMagick::Image.read(current_path) # reopen the img and return
+          img = ::RMagick::Image.read(current_path) # reopen the img and return
         elsif img.class.to_s == "MiniMagick::Image"
           img.write(img.path)
           Piet.pngquant(img.path)
-          ::MiniMagick::Image.open(img.path) # reopen the img and return
+          img = ::MiniMagick::Image.open(img.path) # reopen the img and return
         else
           Piet.pngquant(img.path)
         end
