191d91a2867ea2f7e5f8a8a4c3e262e630f603cc
Julie Cameron <jewlofthelotus@gmail.com>
2015-02-03 10:58:20 -0500

    tests to make sure the proper optimization tool is used based on file MIME type

--- spec/integration/piet_spec.rb
+++ spec/integration/piet_spec.rb
@@ -16,7 +16,13 @@ describe Piet do
       let(:path) { File.join('spec', 'assets', 'piet.jpg') }
       let(:image) { File.open(path) }
 
-      def upload_file(uploader)
+      let(:fake_png_path) { File.join('spec', 'assets', 'fake-png.png') }
+      let(:fake_png_image) { File.open(fake_png_path) }
+
+      let(:real_png_path) { File.join('spec', 'assets', 'real-png.png') }
+      let(:real_png_image) { File.open(real_png_path) }
+
+      def upload_file(uploader, image)
         uploader.store!(image)
         uploader.manipulate! do |img|
           img
@@ -26,7 +32,7 @@ describe Piet do
       it "calls the uploader's `current_path` when MiniMagick is used" do
         uploader = MiniMagickUploader.new
 
-        upload_file(uploader)
+        upload_file(uploader, image)
 
         expect(Piet).to receive(:optimize).with(uploader.current_path, {})
 
@@ -36,9 +42,29 @@ describe Piet do
       it "calls the uploader's `current_path` when RMagick is used" do
         uploader = RMagickUploader.new
 
-        upload_file(uploader)
+        upload_file(uploader, image)
 
         expect(Piet).to receive(:optimize).with(uploader.current_path, {})
+
+        uploader.optimize
+      end
+
+      it "calls `optimize_png` if the file MIME type is that of a PNG" do
+        uploader = MiniMagickUploader.new
+
+        upload_file(uploader, real_png_image)
+
+        expect(Piet).to receive(:optimize_png).with(uploader.current_path, {})
+
+        uploader.optimize
+      end
+
+      it "calls `optimize_jpg` if the file MIME type is that of a JPEG" do
+        uploader = MiniMagickUploader.new
+
+        upload_file(uploader, fake_png_image)
+
+        expect(Piet).to receive(:optimize_jpg).with(uploader.current_path, {})
 
         uploader.optimize
       end
